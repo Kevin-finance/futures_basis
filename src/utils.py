6df51a37422175f76bbcfc26bea9f3bc7995d15c
Kevin-finance <mooseok1007@gmail.com>
2025-05-05 11:49:04 -0500

    utils module update / incorporating pricing.py

---
+++ src/utils.py
@@ -0,0 +1,85 @@
+from datetime import datetime
+from typing import List
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+import polars as pl
+from functools import reduce
+
+def merge_and_filter(
+    dfs: List[pl.DataFrame],
+    on: str,
+    strategy: str,
+    tolerance: str,
+    start: datetime,
+    end: datetime,
+) -> pl.DataFrame:
+    
+    
+    def _join(left, right):
+        return left.join_asof(right, on=on, strategy=strategy, tolerance=tolerance)
+    merged = reduce(_join, dfs)
+    return merged.filter((pl.col(on) >= start) & (pl.col(on) <= end))
+
+def compute_theoretical_prices(df: pl.DataFrame) -> pl.DataFrame:
+    # Spot, Near_Rate, NearMonth_Years, Expected Points 
+    return df.with_columns(
+        (
+            pl.col("Close") * np.exp((pl.col("Near_Rate")/100) * pl.col("NearMonth_Years"))
+            - pl.col("Expected Points")
+        ).alias("Theoretical Futures Price")
+    )
+
+def finalize_columns(df: pl.DataFrame) -> pl.DataFrame:
+    df = df.rename({
+        "Close": "Spot",
+        "Expected Points": "Expected Dividend Points",
+        "Last": "ES Trade",
+        "Last_right": "BTIC",
+    })
+    df = df.with_columns([
+    (pl.col("Theoretical Futures Price") - pl.col("Spot")).alias("Theoretical Basis"),
+    (pl.col("ES Trade") - pl.col("Spot")).alias("Market Basis")])
+
+    return df.with_columns([
+    ((pl.col("Theoretical Basis") * 10000) / pl.col("Spot")).alias("Theoretical Basis Rate"),
+    ((pl.col("Market Basis") * 10000) / pl.col("Spot")).alias("Market Basis Rate"),
+    ((pl.col("BTIC") * 10000) / pl.col("Spot")).alias("BTIC Rate")
+])
+
+def plot_basis(df_pd: pd.DataFrame,in_bps: bool = False):
+    output_pd = df_pd
+    output_pd["UTC-Datetime"] = pd.to_datetime(
+        output_pd["UTC-Datetime"]
+    )
+    color_dict = {
+        "BTIC": dict(color="rgba(100, 200, 100, 0.3)"),
+        "Theoretical Basis": dict(color="rgba(0, 100, 200, 0.3)"),
+        "Market Basis": dict(color="rgba(100, 100, 200, 0.3)"),                              
+    }
+    fig = go.Figure()
+    for k, color in color_dict.items():
+        col = k + " Rate" if in_bps else k
+        fig.add_trace(
+            go.Scatter(
+                x=output_pd["UTC-Datetime"],
+                y=output_pd[col], 
+                mode="lines+markers",
+                name=col,
+                line=color,
+            )
+        )
+    title = "BTIC vs Theoretical Basis vs Market Basis"
+    if in_bps:
+        title = title + " (Rates(bp))"
+    fig.update_layout(
+        title=title,
+        xaxis_title="Time",
+        yaxis_title="Basis Rate" if in_bps else "Index Points",
+        legend_title="Legend",
+        hovermode="x unified",
+        template="plotly_white",
+        height=600,
+    )
+
+    return fig
